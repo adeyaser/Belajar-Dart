cd7fc7a454da119ded626d252da2e39805a43764
adeyaser <59902463+adeyaser@users.noreply.github.com>
2021-06-13 20:10:19 +0700

    perhitungan luas persegi panjang

---
+++ latihan_ke_4_menghitung_luas_persegi.dart
@@ -0,0 +1,11 @@
+void main()
+{
+  //penggunaan variabel double
+  double panjang;
+  double lebar;
+  panjang = 10;
+  lebar = 15.5;
+  double luas = panjang * lebar;
+  print('Luas Persegi Panjang  : ' + luas.toString());
+
+}
