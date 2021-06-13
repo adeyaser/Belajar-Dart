e8ca3cb52f4f6d278afc505a2698d9b741faa5cf
adeyaser <59902463+adeyaser@users.noreply.github.com>
2021-06-13 19:43:33 +0700

    latihan ke 2 variabel -1

---
+++ latihan_ke_2.dart
@@ -0,0 +1,9 @@
+void main()
+{
+  // Penggunaan variabel dengan var
+  var halo ='Hallo world';
+  // penggunaan variabel dengan string
+  String hai ='Hai Selamat Datang di latihan ke 2';
+  print(halo);
+  print(hai);
+}
